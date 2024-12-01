426b18bf18566cb0551f7166485c68fc8fa640c9
JessCP25 <jpayanoc25@gmail.com>
2024-11-30 21:45:46 -0500

    Feat: Implementando login con passport

--- services/user.service.js
+++ services/user.service.js
@@ -19,6 +19,15 @@ class UserService {
     return rta;
   }
 
+  async findByEmail(email) {
+    const user = await models.User.findOne({
+      where: {
+        email: email
+      }
+    });
+    return user;
+  }
+
   async findOne(id) {
     const user = await models.User.findByPk(id);
     if (!user) {
